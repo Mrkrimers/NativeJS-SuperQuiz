95ef1252e66b5284d7970d96e6019765daba1fef
mrkrimers <mrkrimerssasha@gmail.com>
2023-10-11 23:40:22 +0300

    added start JS dropdown menu

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -14,3 +14,37 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
     }
 });
+
+
+const p = document.querySelector('.drop-menu')
+const drop = document.querySelector('.dropdown')
+const li = document.querySelector('.dropdown li')
+let flag = false;
+
+drop.addEventListener('mouseenter', () => {
+    flag = true
+    p.style = `background-color: #9E00FF;
+    transition: 0.15s ease;
+    border-radius: 1em;
+    padding: 12px 32px;
+    box-shadow: 1px 5px 10px -5px $black-color;`
+});
+
+drop.addEventListener('mouseleave', () => {
+    flag = false
+    p.style = 'none';
+    drop.style.display = 'none';
+})
+
+p.addEventListener('mouseenter', () => {
+    drop.style.display = 'block';
+});
+
+p.addEventListener('mouseleave', () => {
+
+    setTimeout(() => {
+        if (!flag) {
+            drop.style.display = 'none';
+        }
+    }, 100);
+});
